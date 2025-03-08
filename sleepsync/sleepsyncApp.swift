48a52857301205b6887302acf43f5079c8d39d07
Fraschetti, Andrea (Stud. DACS) <a.fraschetti@student.maastrichtuniversity.nl>
2025-03-08 15:05:45 +0100

    Initial Commit

---
+++ sleepsync/sleepsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  sleepsyncApp.swift
+//  sleepsync
+//
+//  Created by Andrea Fraschetti on 08/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct sleepsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sleepsyncTests/sleepsyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  sleepsyncTests.swift
+//  sleepsyncTests
+//
+//  Created by Andrea Fraschetti on 08/03/2025.
+//
+
+import Testing
+@testable import sleepsync
+
+struct sleepsyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
